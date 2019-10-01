0e33613b77750b5703cc7848b0e31e9e2c07b930
Carlos Balladares <carlos@balladares.co>
2019-10-01 18:56:02 -0400

    added all files

---
+++ src/entities/form/form.js
@@ -0,0 +1,14 @@
+class form {
+  constructor(definition) {
+    if (
+      !Array.isArray(definition) ||
+      !definition.length ||
+      !this.validForm(definition)
+    ) {
+      throw new Error("Form is not valid");
+    }
+
+    this._definition = definition;
+  }
+}
+export default form;
